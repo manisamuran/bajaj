fb7e38ed4a54ffdb77fcc2a7f590650c53a49b06
manisamuran <92862698+manisamuran@users.noreply.github.com>
2024-03-18 13:18:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post("/bfhl", (req, res) => {
 )
 
 
-
-app.listen(PORT, () => {
-    console.log("Start");
-});
+app.listen(port, '0.0.0.0', err => {
+    if (err) throw err
+    console.log(Listening on port ${PORT})
+  })
